24781b897c04b567bd01cd3ef0504e8a9446f9fe
Joao Vitor Maia <maia.tostring@gmail.com>
2021-09-27 02:03:58 -0300

    Typescript source files added

--- src/commands/index.js
+++
@@ -1,8 +0,0 @@
-import execute from './execute'
-import play from './play'
-import stop from './stop'
-import skip from './skip'
-import queue from './queue'
-import leave from './leave'
-
-export const handlers = { play, execute, stop, skip, queue, leave }
