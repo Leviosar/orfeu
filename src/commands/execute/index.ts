9892f59d3610e38665fa8139925482fa242d291c
João Vitor Maia <maia.tostring@gmail.com>
2021-10-03 17:07:06 -0300

    Support for Youtube playlists added with ytpl

--- src/commands/execute/index.ts
+++ src/commands/execute/index.ts
@@ -2,6 +2,7 @@ import { joinVoiceChannel } from '@discordjs/voice'
 import { Message } from 'discord.js'
 import Youtube from 'ytdl-core'
 import YoutubeSearch, { Video } from 'ytsr'
+import YoutubePlaylist from 'ytpl'
 import Queue from '../../models/queue.js'
 import Song from '../../models/song.js'
 import { BaseCommand } from '../base/base-command.js'
@@ -30,19 +31,83 @@ export class ExecuteCommand implements BaseCommand {
             return message.channel.send("I need permissions to join and speak")
         }
 
-        if (Youtube.validateURL(args[1])) {
-            const info = await Youtube.getBasicInfo(args[1])
-            var song = new Song(info.videoDetails.title, info.videoDetails.video_url)
-        } else {
-            args.shift()
-            const info = await YoutubeSearch(args.join(), {limit: 1})
-            const video: Video = info.items[0] as Video
-            var song = new Song(video.title, video.url)
+        if (Youtube.validateURL(args[1])) /* Direct URL for video */ {
+            this.handleURL(message, queues)
+        } else if (YoutubePlaylist.validateID(args[1])) /* Direct URL for playlist */ {
+            this.handlePlaylistURL(message, queues)
+        } else /* Query string for search */ {
+            this.handleSearch(message, queues)
         }
+    }
+
+    async handleURL(message: Message, queues: Map<string, Queue>) {
+        const args = message.content.split(" ")
+        const info = await Youtube.getBasicInfo(args[1])
+        const song = new Song(info.videoDetails.title, info.videoDetails.video_url)
+        
+        if (await this.addVideoToPlaylist(message, queues, song)) {
+            const queue = queues.get(message.guild.id)
+
+            message.channel.send({
+                embeds: [{
+                    title: song.title,
+                    color: 0,
+                    description: `Was added on the queue. (#${queue.songs.length})`
+                }]
+            })
+        }
+    }
+
+    async handlePlaylistURL(message: Message, queues: Map<string, Queue>) {
+        const args = message.content.split(" ")
+        const info = await YoutubePlaylist(args[1], { limit: Infinity })
+        
+        const songs = info.items.map(item => new Song(item.title, item.url))
+        this.addVideoToPlaylist(message, queues, songs)
+
+        message.channel.send({
+            embeds: [{
+                title: "Playlist added.",
+                color: 0,
+                description: `${info.items.length} songs were added to the queue`
+            }]
+        })
+    }
+
+    async handleSearch(message: Message, queues: Map<string, Queue>) {
+        const args = message.content.split(" ")
+        
+        args.shift()
+        const info = await YoutubeSearch(args.join(), {limit: 1})
+        const video: Video = info.items[0] as Video
+        const song = new Song(video.title, video.url)
+
+        if (await this.addVideoToPlaylist(message, queues, song)) {
+            const queue = queues.get(message.guild.id)
+
+            message.channel.send({
+                embeds: [{
+                    title: song.title,
+                    color: 0,
+                    description: `Was added on the queue. (#${queue.songs.length})`
+                }]
+            })
+        }
+    }
+
+    async addVideoToPlaylist(message: Message, queues: Map<string, Queue>, song: Song | Song[]) : Promise<Boolean> {
+        const queue = queues.get(message.guild.id)
+        const voice = message.member.voice.channel
 
         if (!queue) {
             const queue = new Queue(message)
-            queue.songs.push(song)
+
+            if (song instanceof Song) {
+                queue.songs.push(song)
+            } else {
+                queue.songs = queue.songs.concat(song)
+            }
+
             queues.set(message.guild.id, queue)
 
             try {
@@ -53,14 +118,20 @@ export class ExecuteCommand implements BaseCommand {
                 })
 
                 new PlayCommand().run(message, queues)
+                return true
             } catch (err) {
                 queues.delete(message.guild.id)
-                return message.channel.send("Couldn't join voice channel")
+                message.channel.send("Couldn't join voice channel")
+                return false
             }
         } else {
-            queue.songs.push(song)
-            return message.channel.send(`${song.title} has been added to the queue!`)
+            if (song instanceof Song) {
+                queue.songs.push(song)
+            } else {
+                queue.songs = queue.songs.concat(song)
+            }
+
+            return true
         }
     }
-
 }
